b48f16de6f65cf90255b5d6f14a585ed70f8adce
mengmeng <466556076@qq.com>
2021-04-15 11:55:09 +0800

    init

---
+++ Source/TwoProject/TwoProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TwoProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TWOPROJECT_API ATwoProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
